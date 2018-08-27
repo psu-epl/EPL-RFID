ed6bb006d1994a911666172fdab0267ac5e91b72
Unknown <matty.babaallos@gmail.com>
2018-08-27 12:01:13 -0700

    test setting freq to 120K and 10% duty cycle

---
+++ src/ledc/main/ledc_example_main.c
@@ -0,0 +1,97 @@
+/* LEDC (LED Controller) fade example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/ledc.h"
+#include "esp_err.h"
+
+/*
+ * About this example
+ *
+ * 1. Start with initializing LEDC module:
+ *    a. Set the timer of LEDC first, this determines the frequency
+ *       and resolution of PWM.
+ *    b. Then set the LEDC channel you want to use,
+ *       and bind with one of the timers.
+ *
+ * 2. You need first to install a default fade function,
+ *    then you can use fade APIs.
+ *
+ * 3. You can also set a target duty directly without fading.
+ *
+ * 4. This example uses GPIO18/19/4/5 as LEDC output,
+ *    and it will change the duty repeatedly.
+ *
+ * 5. GPIO18/19 are from high speed channel group.
+ *    GPIO4/5 are from low speed channel group.
+ *
+ */
+#define LEDC_TIMER             LEDC_TIMER_0
+#define LEDC_CHANNEL           LEDC_CHANNEL_0
+#define LEDC_HS_CH0_GPIO       (18)
+
+#define freq                   10000
+
+
+void app_main()
+{
+
+    /*
+     * Prepare and set configuration of timers
+     * that will be used by LED Controller
+     */
+    ledc_timer_config_t ledc_timer = {
+        .duty_resolution = LEDC_TIMER_13_BIT, // resolution of PWM duty
+        .freq_hz = 120000,                      // frequency of PWM signal
+        .speed_mode = LEDC_HIGH_SPEED_MODE,           // timer mode
+        .timer_num = LEDC_TIMER            // timer index
+    };
+    // Set configuration of timer0 for high speed channels
+    ledc_timer_config(&ledc_timer);
+
+
+    /*
+     * Prepare individual configuration
+     * for each channel of LED Controller
+     * by selecting:
+     * - controller's channel number
+     * - output duty cycle, set initially to 0
+     * - GPIO number where LED is connected to
+     * - speed mode, either high or low
+     * - timer servicing selected channel
+     *   Note: if different channels use one timer,
+     *         then frequency and bit_num of these channels
+     *         will be the same
+     */
+    ledc_channel_config_t ledc_channel =  {
+            .channel    = LEDC_CHANNEL,
+            .duty       = 0,
+            .gpio_num   = LEDC_HS_CH0_GPIO,
+            .speed_mode = LEDC_HIGH_SPEED_MODE,
+            .timer_sel  = LEDC_TIMER
+        };
+
+
+    ledc_channel_config(&ledc_channel);
+
+
+    while(1){
+        
+        ledc_set_duty(LEDC_HIGH_SPEED_MODE,LEDC_CHANNEL, 10);
+        ledc_update_duty(LEDC_HIGH_SPEED_MODE,LEDC_CHANNEL);
+        printf("1. LEDC duty cycle set to = %d\n", ledc_get_freq(LEDC_HIGH_SPEED_MODE,LEDC_TIMER) );
+        printf("1. LEDC duty cycle set to = %d\n", ledc_get_duty(LEDC_HIGH_SPEED_MODE,LEDC_CHANNEL) );
+
+        vTaskDelay(1000 / portTICK_PERIOD_MS);        
+    }
+
+
+
+}
