5c4559f2cd904da10a141b57e68106cafc8c1219
Unknown <matty.babaallos@gmail.com>
2018-09-16 03:49:00 -0700

    can detect a card now

--- src/esp32/spi/main/main.c
+++ src/esp32/spi/main/main.c
@@ -1,46 +1,54 @@
 #include "rc522.h"
 #include "esp_log.h"
 
-
-
 #define READER13 "13.5 MHz reader"
 
 static spi_device_handle_t spi;
 
-
 void app_main(void)
 {
-  esp_err_t ret;
-  spi_bus_config_t buscfg = {
-      .miso_io_num = PIN_NUM_MISO,
-      .mosi_io_num = PIN_NUM_MOSI,
-      .sclk_io_num = PIN_NUM_CLK,
-      .quadwp_io_num = -1,
-      .quadhd_io_num = -1,
-  };
+    esp_err_t ret;
+    spi_bus_config_t buscfg = {
+        .miso_io_num = PIN_NUM_MISO,
+        .mosi_io_num = PIN_NUM_MOSI,
+        .sclk_io_num = PIN_NUM_CLK,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+    };
 
-  //Device configuration struct
-  spi_device_interface_config_t devcfg = {
-      .clock_speed_hz = 10 * 1000 * 1000, //Clock out at 10 MHz
-      .mode = 0,                          //SPI mode 0
-      .spics_io_num = RC522_PIN_CS,         //CS pin
-      .queue_size = 7,                    //We want to be able to queue 7 transactions at a time
-      .command_bits = 0,                  //The number of command bits for the device
-      .address_bits = 8                   //The number of bit used for the address
-  };
+    //Device configuration struct
+    spi_device_interface_config_t devcfg = {
+        .clock_speed_hz = 10 * 1000 * 1000, //Clock out at 10 MHz
+        .mode = 0,                          //SPI mode 0
+        .spics_io_num = RC522_PIN_CS,       //CS pin
+        .queue_size = 7,                    //We want to be able to queue 7 transactions at a time
+        .command_bits = 0,                  //The number of command bits for the device
+        .address_bits = 8                   //The number of bit used for the address
+    };
 
-  //Initialize the SPI bus
-  ret = spi_bus_initialize(HSPI_HOST, &buscfg, 0); //The 0 is to disable DMA
-  ESP_ERROR_CHECK(ret);
+    //Initialize the SPI bus
+    ret = spi_bus_initialize(HSPI_HOST, &buscfg, 0); //The 0 is to disable DMA
+    ESP_ERROR_CHECK(ret);
 
-  //Attach the LCD to the SPI bus
-  ret = spi_bus_add_device(HSPI_HOST, &devcfg, &spi);
-  ESP_ERROR_CHECK(ret);
+    //Attach the LCD to the SPI bus
+    ret = spi_bus_add_device(HSPI_HOST, &devcfg, &spi);
+    ESP_ERROR_CHECK(ret);
 
-  RC522_init();
+    RC522_init();
+    uint8_t bufferATQA[3] = {0, 0, 0};
+    uint8_t bufferSize = sizeof(bufferATQA);
+    uint8_t result;
+
+    while (1)
+    {
+        bufferSize = 3;
+        result = RC522_REQA_or_WUPA(PICC_CMD_REQA, &bufferATQA, &bufferSize);
+        if (result == STATUS_OK || result == STATUS_COLLISION)
+            ESP_LOGW(READER13, "Card detected!!!");
+        
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
 }
-
-
 
 /*
 Note:
@@ -52,56 +60,148 @@ in the data being sent.
 //Write out to a register
 void write_reg(uint8_t add, uint8_t data)
 {
-  esp_err_t ret;
-  spi_transaction_t t;
-  memset(&t, 0, sizeof(t)); //Zero out the transaction
-  t.addr = add<<1;             //MSB == 0 is for writing from Datasheet section 8.1.2.3.
-  t.tx_buffer = (void *)&data;
-  t.length = 8;              //This is the length of the tx_buffer in bits
-  ret = spi_device_transmit(spi, &t); //Transmit!
-  assert(ret == ESP_OK);              //Should have had no issues.
+    esp_err_t ret;
+    spi_transaction_t t;
+    memset(&t, 0, sizeof(t)); //Zero out the transaction
+    t.addr = add << 1;        //MSB == 0 is for writing from Datasheet section 8.1.2.3.
+    t.tx_buffer = (void *)&data;
+    t.length = 8;                       //This is the length of the tx_buffer in bits
+    ret = spi_device_transmit(spi, &t); //Transmit!
+    assert(ret == ESP_OK);              //Should have had no issues.
+}
+
+void write_reg_array(uint8_t add, uint8_t *data, uint8_t data_len)
+{
+    esp_err_t ret;
+    spi_transaction_t t;
+    memset(&t, 0, sizeof(t)); //Zero out the transaction
+    t.addr = add << 1;        //MSB == 0 is for writing from Datasheet section 8.1.2.3.
+    t.tx_buffer = (void *)&data;
+    t.length = 8 * data_len;            //This is the length of the tx_buffer in bits
+    ret = spi_device_transmit(spi, &t); //Transmit!
+    assert(ret == ESP_OK);              //Should have had no issues.
 }
 
 //Read register and return its value
 uint8_t read_reg(uint8_t add)
 {
-  esp_err_t ret;
-  spi_transaction_t t;
-  uint8_t buffer;
-  memset(&t, 0, sizeof(t)); //Zero out the transaction
-  t.addr = (0x80|(add<<1));             //MSB == 1 is for reading from Datasheet section 8.1.2.3.
-  t.length = 8;
-  t.rx_buffer = (void *)&buffer;
-  ret = spi_device_transmit(spi, &t); //Transmit!
-  assert(ret == ESP_OK);              //Should have had no issues.
-  /*
+    esp_err_t ret;
+    spi_transaction_t t;
+    uint8_t buffer;
+    memset(&t, 0, sizeof(t));     //Zero out the transaction
+    t.addr = (0x80 | (add << 1)); //MSB == 1 is for reading from Datasheet section 8.1.2.3.
+    t.length = 8;
+    t.rx_buffer = (void *)&buffer;
+    ret = spi_device_transmit(spi, &t); //Transmit!
+    assert(ret == ESP_OK);              //Should have had no issues.
+    /*
     We need the second transmit because
     on the transmit we don't get anything from RC522
     this is defined section 8.1.2.1
   */
-  ret = spi_device_transmit(spi, &t); //Transmit!
-  assert(ret == ESP_OK);              //Should have had no issues.
-  return buffer;
+    ret = spi_device_transmit(spi, &t); //Transmit!
+    assert(ret == ESP_OK);              //Should have had no issues.
+    return buffer;
+}
+
+//Read register and return its value
+void read_reg_array(uint8_t add, uint8_t *data, uint8_t data_len)
+{
+    esp_err_t ret;
+    spi_transaction_t t;
+    memset(&t, 0, sizeof(t));     //Zero out the transaction
+    t.addr = (0x80 | (add << 1)); //MSB == 1 is for reading from Datasheet section 8.1.2.3.
+    t.length = 8 * data_len;
+    t.rx_buffer = (void *)data;
+    ret = spi_device_transmit(spi, &t); //Transmit!
+    assert(ret == ESP_OK);              //Should have had no issues.
+    /*
+    We need the second transmit because
+    on the transmit we don't get anything from RC522
+    this is defined section 8.1.2.1
+  */
+    ret = spi_device_transmit(spi, &t); //Transmit!
+    assert(ret == ESP_OK);              //Should have had no issues.
 }
 
 void RC522_init()
 {
     RC522_reset();
+    /*
+       Setup timeout timer
+        This timer starts when the PCD stop transmission.
+        Later we can get the IRQ Reg to see if the timer went off before a complete command
+    */
+    write_reg(TModeReg, 0x80); //Timer start automatically at end of transmission
 
-
-    //Setup timeout timer
-    write_reg(TModeReg,0x80); //Timer start automatically at end of transmission
-
-    write_reg(TPrescalerReg,0xA9); // Tprescaler LSB 7 bits of divider
-    write_reg(TReloadRegL,0xE8); //time reload value, lower 8 bits 
-    write_reg(TReloadRegH,0x03); // timer reload value, higher 8 bits
+    write_reg(TPrescalerReg, 0xA9); // Tprescaler LSB 7 bits of divider
+    write_reg(TReloadRegL, 0xE8);   //time reload value, lower 8 bits
+    write_reg(TReloadRegH, 0x03);   // timer reload value, higher 8 bits
 
     /* forces a 100% ASK modulation independent of modGsPReg  (Type-A)
      * ASK = Amplitude Shift Keying */
-    write_reg(TxASKReg,0x40);
-    ESP_LOGI(READER13,"Done Initializing");
+    write_reg(TxASKReg, 0x40);
+    write_reg(ModeReg, 0x3D);
 
+    RC522_antenna_on();
+    ESP_LOGI(READER13, "Done Initializing");
+}
 
+uint8_t RC522_REQA_or_WUPA(uint8_t card_command, uint8_t *bufferATQA, uint8_t *bufferSize)
+{
+    uint8_t status;
+    if (bufferATQA == NULL || *bufferSize < 2)
+        return STATUS_NO_ROOM;
+
+    clear_bits(CollReg, 0x80); // ValuesAfterColl=1 => Bits received after collision are cleared.
+    status = RC522_communicate_with_card(PCD_TRANSCEIVE, 0x30, &card_command, 1, bufferATQA, bufferSize, 7);
+    if (status != STATUS_OK)
+        return status;
+
+    if (*bufferSize > 2) // ATQA must be exactly 16 bits.
+        return STATUS_ERROR;
+
+    return STATUS_OK;
+}
+
+uint8_t RC522_communicate_with_card(uint8_t command, uint8_t irq, uint8_t *data_out, uint8_t data_out_len, uint8_t *data_in, uint8_t *data_in_len, uint8_t tx_last_bits)
+{
+
+    write_reg(CommandReg, PCD_IDLE);                      // Stop any active command.
+    write_reg(ComIrqReg, 0x7F);                           // Clear all seven interrupt request bits
+    write_reg(FIFOLevelReg, 0x80);                        // FlushBuffer = 1, FIFO initialization  
+    write_reg(FIFODataReg, data_out[0]); //Write data to FIFO
+    set_bits(BitFramingReg, tx_last_bits);                // Set the number of bits to be transmitted for the last command
+    write_reg(CommandReg, command);                       //Execute the command
+    
+
+    if (command == PCD_TRANSCEIVE) //Start transmission of transceive see 9.3.1.14
+        set_bits(BitFramingReg, 0x80);
+
+    vTaskDelay(30 / portTICK_PERIOD_MS); //Sleep for 40 ms for the data to be sent out
+
+    uint8_t status = read_reg(ComIrqReg);
+
+    if ((status & 0x01) && !(status & irq)) //Check if a timeout happened from the timer and the waiting IRQ didn't happen
+        return STATUS_TIMEOUT;
+   
+    uint8_t error_code = read_reg(ErrorReg);
+    if (error_code & 0x13)
+        return STATUS_ERROR;
+
+    //return data
+    if (command == PCD_TRANSCEIVE || command == PCD_RECEIVE)
+    {
+        uint8_t len = read_reg(FIFOLevelReg);
+
+        *data_in_len = len;
+        read_reg_array(FIFODataReg, data_in, len);
+    }
+
+    if (error_code & 0x08) // Check if bit collisions  CollErr
+        return STATUS_COLLISION;
+
+    return STATUS_OK;
 }
 
 //Perform soft reset on the reader
@@ -110,12 +210,11 @@ void RC522_reset()
     /*
         See section 9.3.1.2 of the datasheet about Softreset
     */
-    write_reg(CommandReg,PCD_RESET); // Softreset the chip;
-    while(read_reg(CommandReg) & 0x10); //Wait to exit PowerDown
-    ESP_LOGI(READER13,"Powered up and ready");
-
+    write_reg(CommandReg, PCD_RESET); // Softreset the chip;
+    while (read_reg(CommandReg) & 0x10)
+        ; //Wait to exit PowerDown
+    ESP_LOGI(READER13, "Powered up and ready");
 }
-
 
 /* Antenna Control 
  Section 8.6.3 of the datasheet 
@@ -124,15 +223,14 @@ void RC522_antenna_on()
 {
     //Check if it's not on
     if (!(read_reg(TxControlReg) & 0x03))
-            set_bits(TxControlReg, 0x03);
-    ESP_LOGI(READER13,"Antenna is on");
+        set_bits(TxControlReg, 0x03);
+    ESP_LOGI(READER13, "Antenna is on");
 }
 
 void RC522_antenna_off()
 {
     clear_bits(TxControlReg, 0x03);
-    ESP_LOGI(READER13,"Antenna is on");
-
+    ESP_LOGI(READER13, "Antenna is on");
 }
 
 //Clear bits marked by the mask in a given register
--- src/esp32/spi/main/rc522.h
+++ src/esp32/spi/main/rc522.h
@@ -10,77 +10,6 @@
 #include "soc/gpio_struct.h"
 #include "driver/gpio.h"
 
-
-
-//MF522 commands for the CommandReg
-#define PCD_IDLE              0x00		// no action, cancels current command execution
-#define PCD_AUTHENT           0x0E		// perform the MIFARE standard authentication as a reader
-#define PCD_RECEIVE           0x08		// activate the receiver circuits
-#define PCD_TRANSMIT          0x04		// transmits data from the FIFO buffer
-#define PCD_TRANSCEIVE        0x0C		// transmits data from the FIFO buffer & activate receive after transmission
-#define PCD_RESET             0x0F		// softreset command
-#define PCD_CALCCRC           0x03		// activates the CRC co-processor or perform a selftest
-#define PCD_NOCHANGE          0x05      // no command change. To modify the CommandReg register bits
-
-
-/** MF522 registers */
-// command and status
-#define     CommandReg            0x01	// starts and stops command execution
-#define     ComIEnReg             0x02	// enable and disable interrupt request control bits
-#define     DivlEnReg             0x03	// enable and disable interrupt request control bits
-#define     ComIrqReg             0x04	// interrupt request bits
-#define     DivIrqReg             0x05	// interrupt request bits
-#define     ErrorReg              0x06	// error bits showing the error status of last command
-#define     Status1Reg            0x07	// communication status bits
-#define     Status2Reg            0x08	// receiver and transmitter status bits
-#define     FIFODataReg           0x09	// input and output of 64 byte FIFO buffer
-#define     FIFOLevelReg          0x0A	// number of bytes stored in the FIFO buffer
-#define     WaterLevelReg         0x0B	// level for FIFO underflow and overflow warning
-#define     ControlReg            0x0C	// miscellaneus control reigsters
-#define     BitFramingReg         0x0D	// adjustments for bit-oriented frames
-#define     CollReg               0x0E	// bit position of the first bit-collision detected on RF int.
-// command
-#define     ModeReg               0x11	// defines general modes for transmitting and receiving
-#define     TxModeReg             0x12	// defines transmission data rate and framing
-#define     RxModeReg             0x13	// defines reception data rate and framing
-#define     TxControlReg          0x14	// controls the logical behavior of the antenna driver TX1/ TX2
-#define     TxASKReg              0x15	// controls the setting of the transmission modulation
-#define     TxSelReg              0x16	// selects the internal sources for the antenna driver
-#define     RxSelReg              0x17	// selects the internal receiver settings
-#define     RxThresholdReg        0x18	// selects thresholds for the bit decoder
-#define     DemodReg              0x19	// defines demodulator settings
-#define     MiTXReg               0x1C	// controls some MIFARE communication transmit parameters
-#define     MiRXReg               0x1D	// controls some MIFARE communication receive parameters
-#define     SerialSpeedReg        0x1F	// selects the speed for the serial UART interface
-// configuration
-#define     CRCResultRegM         0x21	// shows the MSB value for the CRC calculation
-#define     CRCResultRegL         0x22	// shows the LSB value for the CRC calculation
-#define     ModWidthReg           0x24	// controls the ModWidth settings
-#define     RFCfgReg              0x26	// configures the receiver gain
-#define     GsNReg                0x27	// selects the conductance of the antenna on TX1/TX2 modulation
-#define     CWGsCfgReg            0x28	// defines the conductance of the p-driver output
-#define     ModGsCfgReg           0x29  // defines the conductance of the p-driver output
-#define     TModeReg              0x2A  // defines settings for the internal timer
-#define     TPrescalerReg         0x2B  // defines settings for the internal timer
-#define     TReloadRegH           0x2C  // defines the 16 bit timer reload value
-#define     TReloadRegL           0x2D  // defines the 16 bit timer reload value
-#define     TCounterValueRegH     0x2E  // shows the 16 bit timer reload value
-#define     TCounterValueRegL     0x2F  // shows the 16 bit timer reload value
-// test register
-#define     TestSel1Reg           0x31  // general test signal configuration
-#define     TestSel2Reg           0x32  // general test signal configuration and PRBS control
-#define     TestPinEnReg          0x33	// enables pin output driver on pins D1 to D7
-#define     TestPinValueReg       0x34	// defines values for D1 to D7 when used as I/O bus
-#define     TestBusReg            0x35	// shows the status of the internal test bus
-#define     AutoTestReg           0x36	// controls the digital self test
-#define     VersionReg            0x37	// shows the software version
-#define     AnalogTestReg         0x38	// controls the pins AUX1 and AUX2
-#define     TestDAC1Reg           0x39	// defines the test value for testDAC1
-#define     TestDAC2Reg           0x3A  // defines the test value for testDAC2
-#define     TestADCReg            0x3B	// shows the value of ADC I and Q channels
-
-
-
 //ESP32 WROOM HSPI Pins
 #define PIN_NUM_MISO 12
 #define PIN_NUM_MOSI 13
@@ -89,16 +18,127 @@
 //RC522 Chip Select
 #define RC522_PIN_CS 15
 
+//MF522 commands for the CommandReg
+#define PCD_IDLE 0x00       // no action, cancels current command execution
+#define PCD_AUTHENT 0x0E    // perform the MIFARE standard authentication as a reader
+#define PCD_RECEIVE 0x08    // activate the receiver circuits
+#define PCD_TRANSMIT 0x04   // transmits data from the FIFO buffer
+#define PCD_TRANSCEIVE 0x0C // transmits data from the FIFO buffer & activate receive after transmission
+#define PCD_RESET 0x0F      // softreset command
+#define PCD_CALCCRC 0x03    // activates the CRC co-processor or perform a selftest
+#define PCD_NOCHANGE 0x05   // no command change. To modify the CommandReg register bits
+
+/** MF522 registers */
+// command and status
+#define CommandReg 0x01    // starts and stops command execution
+#define ComIEnReg 0x02     // enable and disable interrupt request control bits
+#define DivlEnReg 0x03     // enable and disable interrupt request control bits
+#define ComIrqReg 0x04     // interrupt request bits
+#define DivIrqReg 0x05     // interrupt request bits
+#define ErrorReg 0x06      // error bits showing the error status of last command
+#define Status1Reg 0x07    // communication status bits
+#define Status2Reg 0x08    // receiver and transmitter status bits
+#define FIFODataReg 0x09   // input and output of 64 byte FIFO buffer
+#define FIFOLevelReg 0x0A  // number of bytes stored in the FIFO buffer
+#define WaterLevelReg 0x0B // level for FIFO underflow and overflow warning
+#define ControlReg 0x0C    // miscellaneus control reigsters
+#define BitFramingReg 0x0D // adjustments for bit-oriented frames
+#define CollReg 0x0E       // bit position of the first bit-collision detected on RF int.
+// command
+#define ModeReg 0x11        // defines general modes for transmitting and receiving
+#define TxModeReg 0x12      // defines transmission data rate and framing
+#define RxModeReg 0x13      // defines reception data rate and framing
+#define TxControlReg 0x14   // controls the logical behavior of the antenna driver TX1/ TX2
+#define TxASKReg 0x15       // controls the setting of the transmission modulation
+#define TxSelReg 0x16       // selects the internal sources for the antenna driver
+#define RxSelReg 0x17       // selects the internal receiver settings
+#define RxThresholdReg 0x18 // selects thresholds for the bit decoder
+#define DemodReg 0x19       // defines demodulator settings
+#define MiTXReg 0x1C        // controls some MIFARE communication transmit parameters
+#define MiRXReg 0x1D        // controls some MIFARE communication receive parameters
+#define SerialSpeedReg 0x1F // selects the speed for the serial UART interface
+// configuration
+#define CRCResultRegM 0x21     // shows the MSB value for the CRC calculation
+#define CRCResultRegL 0x22     // shows the LSB value for the CRC calculation
+#define ModWidthReg 0x24       // controls the ModWidth settings
+#define RFCfgReg 0x26          // configures the receiver gain
+#define GsNReg 0x27            // selects the conductance of the antenna on TX1/TX2 modulation
+#define CWGsCfgReg 0x28        // defines the conductance of the p-driver output
+#define ModGsCfgReg 0x29       // defines the conductance of the p-driver output
+#define TModeReg 0x2A          // defines settings for the internal timer
+#define TPrescalerReg 0x2B     // defines settings for the internal timer
+#define TReloadRegH 0x2C       // defines the 16 bit timer reload value
+#define TReloadRegL 0x2D       // defines the 16 bit timer reload value
+#define TCounterValueRegH 0x2E // shows the 16 bit timer reload value
+#define TCounterValueRegL 0x2F // shows the 16 bit timer reload value
+// test register
+#define TestSel1Reg 0x31     // general test signal configuration
+#define TestSel2Reg 0x32     // general test signal configuration and PRBS control
+#define TestPinEnReg 0x33    // enables pin output driver on pins D1 to D7
+#define TestPinValueReg 0x34 // defines values for D1 to D7 when used as I/O bus
+#define TestBusReg 0x35      // shows the status of the internal test bus
+#define AutoTestReg 0x36     // controls the digital self test
+#define VersionReg 0x37      // shows the software version
+#define AnalogTestReg 0x38   // controls the pins AUX1 and AUX2
+#define TestDAC1Reg 0x39     // defines the test value for testDAC1
+#define TestDAC2Reg 0x3A     // defines the test value for testDAC2
+#define TestADCReg 0x3B      // shows the value of ADC I and Q channels
+
+// Commands sent to the PICC.
+
+// The commands used by the PCD to manage communication with several PICCs (ISO 14443-3, Type A, section 6.4)
+#define PICC_CMD_REQA 0x26   // REQuest command Type A. Invites PICCs in state IDLE to go to READY and prepare for anticollision or selection. 7 bit frame.
+#define PICC_CMD_WUPA 0x52    // Wake-UP command, Type A. Invites PICCs in state IDLE and HALT to go to READY(*) and prepare for anticollision or selection. 7 bit frame.
+#define PICC_CMD_CT 0x88      // Cascade Tag. Not really a command, but used during anti collision.
+#define PICC_CMD_SEL_CL1 0x93 // Anti collision/Select, Cascade Level 1
+#define PICC_CMD_SEL_CL2 0x95 // Anti collision/Select, Cascade Level 2
+#define PICC_CMD_SEL_CL3 0x97 // Anti collision/Select, Cascade Level 3
+#define PICC_CMD_HLTA 0x50   // HaLT command, Type A. Instructs an ACTIVE PICC to go to state HALT.
+#define PICC_CMD_RATS 0xE0    // Request command for Answer To Reset.
+// The commands used for MIFARE Classic (from http://www.mouser.com/ds/2/302/MF1S503x-89574.pdf, Section 9)
+// Use PCD_MFAuthent to authenticate access to a sector, then use these commands to read/write/modify the blocks on the sector.
+// The read/write commands can also be used for MIFARE Ultralight.
+#define PICC_CMD_MF_AUTH_KEY_A = 0x60 // Perform authentication with Key A
+#define PICC_CMD_MF_AUTH_KEY_B = 0x61 // Perform authentication with Key B
+#define PICC_CMD_MF_READ = 0x30      // Reads one 16 byte block from the authenticated sector of the PICC. Also used for MIFARE Ultralight.
+#define PICC_CMD_MF_WRITE = 0xA0     // Writes one 16 byte block to the authenticated sector of the PICC. Called "COMPATIBILITY WRITE" for MIFARE Ultralight.
+#define PICC_CMD_MF_DECREMENT = 0xC0  // Decrements the contents of a block and stores the result in the internal data register.
+#define PICC_CMD_MF_INCREMENT = 0xC1  // Increments the contents of a block and stores the result in the internal data register.
+#define PICC_CMD_MF_RESTORE = 0xC2    // Reads the contents of a block into the internal data register.
+#define PICC_CMD_MF_TRANSFER = 0xB0   // Writes the contents of the internal data register to a block.
+                                       // The commands used for MIFARE Ultralight (from http://www.nxp.com/documents/data_sheet/MF0ICU1.pdf, Section 8.6)
+                                       // The PICC_CMD_MF_READ and PICC_CMD_MF_WRITE can also be used for MIFARE Ultralight.
+#define PICC_CMD_UL_WRITE = 0xA2       // Writes one 4 byte page to the PICC.
+
+// Return codes from the functions
+enum StatusCode
+{
+    STATUS_OK,             // Success
+    STATUS_ERROR,          // Error in communication
+    STATUS_COLLISION,      // Collission detected
+    STATUS_TIMEOUT,        // Timeout in communication.
+    STATUS_NO_ROOM,        // A buffer is not big enough.
+    STATUS_INTERNAL_ERROR, // Internal error in the code. Should not happen ;-)
+    STATUS_INVALID,        // Invalid argument.
+    STATUS_CRC_WRONG,      // The CRC_A does not match
+    STATUS_MIFARE_NACK     // A MIFARE PICC responded with NAK.
+};
 
 
 
-void write_out(uint8_t add, uint8_t data);
+void write_reg(uint8_t add, uint8_t data);
+void write_reg_array(uint8_t add, uint8_t *data, uint8_t data_len);
+
 uint8_t read_reg(uint8_t add);
+void read_reg_array(uint8_t add, uint8_t *data, uint8_t data_len);
 
 void set_bits(uint8_t reg, uint8_t mask);
 void clear_bits(uint8_t reg, uint8_t mask);
 
+uint8_t RC522_communicate_with_card(uint8_t command, uint8_t irq, uint8_t *data_out, uint8_t data_out_len, uint8_t *data_in, uint8_t *data_in_len, uint8_t tx_last_bits);
+uint8_t RC522_REQA_or_WUPA(uint8_t card_command, uint8_t *bufferATQA, uint8_t *bufferSize);
+void RC522_antenna_on();
+void RC522_antenna_off();
+
 void RC522_reset();
 void RC522_init();
-
-
